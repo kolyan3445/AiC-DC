bd700bcdc9c9876f15513f5025a2939a6234ee48
kolyan3445 <55030059+kolyan3445@users.noreply.github.com>
2023-05-17 21:51:38 +0400

    Add files via upload

--- labwerk.py
+++ labwerk.py
@@ -1,39 +1,53 @@
 #Написать программу, которая читая символы из бесконечной последовательности (эмулируется конечным файлом, читающимся поблочно), распознает, преобразует и выводит на экран числа по определенному правилу. Числа распознаются по законам грамматики русского языка. Преобразование делать по возможности через словарь. Для упрощения под выводом числа прописью подразумевается последовательный вывод всех цифр числа. Регулярные выражения использовать нельзя.
 #Натуральные числа, состоящие из цифр в порядке возрастания. Для каждого числа минимальную и максимальную цифру вывести прописью.
 
-dict = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
-        '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
+word_dict = {
+    '1': 'один',
+    '2': 'два',
+    '3': 'три',
+    '4': 'четыре',
+    '5': 'пять',
+    '6': 'шесть',
+    '7': 'семь',
+    '8': 'восемь',
+    '9': 'девять',
+    '0': 'ноль'
+}
 
+# Открытие файла
+filename = 'test1.txt'
+block_size = 100
 
-def num_to_text(number):
-    return dict[number]  # словарь
+with open(filename, 'r') as f:
+    # Поблочное чтение из файла
+    while True:
+        block = f.read(block_size)
+        if not block:
+            break
 
+        # Разбитие блока
+        chars = list(block)
+        i = 0
+        while i < len(chars):
+            # Проверка на число
+            if chars[i].isdigit():
+                num_str = chars[i]
+                i += 1
+                while i < len(chars) and chars[i].isdigit():
+                    num_str += chars[i]
+                    i += 1
 
-buffer_size = 1
-work_buffer = ''
-numbers = []
+                # Проверка на возрастание
+                is_ascending = True
+                for j in range(len(num_str) - 1):
+                    if int(num_str[j]) >= int(num_str[j + 1]):
+                        is_ascending = False
 
-with open("test1.txt") as file:  # открытие файла
-    buffer = file.read(buffer_size)  # добавление файла в буфер
-    if not buffer:
-        print("\nФайл test1.txt пустой\nДобавьте не пустой или переименуйте существующий .txt файл.")
-    while buffer:
-        while '0' <= buffer <= '9':
-            work_buffer += buffer
-            buffer = file.read(buffer_size)
-        if work_buffer:
-            for i in range(len(work_buffer)):
-                try:
-                    if work_buffer[i] < work_buffer[i + 1]:
-                        pass
-                    else:
-                        break
-                except IndexError:
-                    numbers.append(work_buffer)
-                    break
-        work_buffer = ''
-        buffer = file.read(buffer_size)
+                # Перевод в слова
+                if is_ascending:
+                    min_digit = word_dict[min(num_str)]
+                    max_digit = word_dict[max(num_str)]
+                    print(num_str + ': ' + min_digit + ', ' + max_digit)
 
-for i in numbers:
-    ans = ' min = ' + num_to_text(i[0]) + ', max = ' + num_to_text(i[len(i) - 1])
-    print(i + ' - ' + ans)
+            else:
+                i += 1
